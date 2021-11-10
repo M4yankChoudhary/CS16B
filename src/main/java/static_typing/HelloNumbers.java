4158cf611489418d91c63852e42bccd66e7e67d9
Mayank Choudhary <7mayankchoudhary7@gmail.com>
2021-11-10 13:06:09 +0530

    Static Typing

---
+++ src/main/java/static_typing/HelloNumbers.java
@@ -0,0 +1,11 @@
+package static_typing;
+
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            System.out.print(x);
+            x++;
+        }
+    }
+}
